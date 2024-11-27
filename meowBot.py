002dba11ba905b5183a15edea1f3199309ba2f61
jewelsonmyjeans <aviadchuraman@gmail.com>
2024-11-27 16:14:47 -0500

    idk

--- meowBot.py
+++ meowBot.py
@@ -7,29 +7,41 @@
 import os
 import discord
 from discord.ext import commands
+from discord import app_commands
 from dotenv import load_dotenv
 
-# Load environment variables
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-# Set up intents explicitly
 intents = discord.Intents.default()
 intents.message_content = True 
 intents.guilds = True
 intents.guild_messages = True
 
-# Create bot instance
-bot = commands.Bot(command_prefix='/', intents=intents)
+GUILD_ID = discord.Object(651263305796681748)
 
-@bot.event
-async def on_ready():
-    print(f'{bot.user} is now online!')
-    await bot.change_presence(activity=discord.Streaming(name="Meow ^-^", url='https://www.twitch.tv/iraqs'))
+class Client(commands.Bot):
+    def __init__(self):
+        super().__init__(command_prefix="!", intents=discord.Intents.default())
+        
+    async def setup_hook(self):
+        await self.tree.sync(guild=GUILD_ID)
+        
+    async def on_ready(self):
+        print(f'{self.user} is now online!')
+        await self.change_presence(activity=discord.Streaming(name="Meow ^-^", url='https://www.twitch.tv/iraqs'))
 
-@bot.command(name='hello')
-async def hello(ctx):
-    await ctx.send(f'Hello ^-^ {ctx.author.name}! 😺')
+
+bot = Client()
+
+@bot.tree.command(name='hello',description="hai", guild=GUILD_ID)
+async def hello(interaction: discord.Interaction):
+    await interaction.response.send_message(f'Hello ^-^ {interaction.user.name}! 😺')
+    
+@bot.tree.command(name="meow", description="meow",guild=GUILD_ID)
+async def meow(interaction: discord.Interaction):
+    await interaction.response.send_message(f"{interaction.user.name} meow")
+
 
 if __name__ == "__main__":
     bot.run(TOKEN)
