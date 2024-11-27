d15ceab837fc315066e395e243f74f6d168cc113
jewelsonmyjeans <aviadchuraman@gmail.com>
2024-11-27 11:17:22 -0500

    readme and just some random testing

--- meowBot.py
+++ meowBot.py
@@ -1,3 +1,9 @@
+# Ideas for this bot
+# Blackjack and economy for blackjack, gifs, crypto tracker, server info
+# Force move all to one channel
+# Gifs: slap, kiss, smack, cat, etc.
+
+
 import os
 import discord
 from discord.ext import commands
@@ -9,7 +15,7 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 
 # Set up intents explicitly
 intents = discord.Intents.default()
-intents.message_content = True  # This is the privileged intent we need
+intents.message_content = True 
 intents.guilds = True
 intents.guild_messages = True
 
@@ -19,11 +25,11 @@ bot = commands.Bot(command_prefix='/', intents=intents)
 @bot.event
 async def on_ready():
     print(f'{bot.user} is now online!')
-    await bot.change_presence(activity=discord.Game(name="Meow! 🐱"))
+    await bot.change_presence(activity=discord.Streaming(name="Meow ^-^", url='https://www.twitch.tv/iraqs'))
 
 @bot.command(name='hello')
 async def hello(ctx):
-    await ctx.send(f'Hello {ctx.author.name}! 😺')
+    await ctx.send(f'Hello ^-^ {ctx.author.name}! 😺')
 
 if __name__ == "__main__":
     bot.run(TOKEN)
